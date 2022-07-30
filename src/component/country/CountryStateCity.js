b48f7553aaab5d316833f1755859cf3c9e0b3990
log101 <frknerdm@mail.com>
2022-07-30 13:48:40 +0300

    add state selector

--- src/component/country/CountryStateCity.js
+++ src/component/country/CountryStateCity.js
@@ -1,76 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
-import {Form} from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import Select from "react-select";
-import { Country, State, City }  from 'country-state-city';
-import csc from "country-state-city";
+import { Country, State, City } from 'country-state-city';
 
 export default function CountryStateCity() {
-    const addressFromik = useFormik({
-        initialValues: {
-        country: "US",
-        state: null,
-        city: null
-        },
-        onSubmit: (values) => console.log(JSON.stringify(values))
-    });
+  const handleStateSelect = (event) => setState(event.target.value)
+  const handleCitySelect = (event) => setSelectedCity(event.target.value)
 
-    const states = State.getStatesOfCountry(addressFromik.initialValues.country)
-    
-    // update states
-    const updatedStates = states.map((countryId) => ({
-        label: countryId.name,
-        value: countryId.isoCode, ...countryId
-    }));
-    const { values, handleSubmit, setFieldValue, setValues } = addressFromik;
+  const states = State.getStatesOfCountry("US")
 
-    const cities = City.getCitiesOfState(addressFromik.initialValues.country, addressFromik.initialValues.state)
-    
-    const updatedCities = (stateId) => 
-        City.getCitiesOfState(stateId)
-        .map((city) => ({
-            label: city.name,
-            value: city.isoCode, ...city
-        }))
-    
-    console.log(City.getCitiesOfState('US', 'AZ'))
+  const [cities, setCities] = useState([])
+  const [selectedCity, setSelectedCity] = useState("")
+  const [state, setState] = useState("AZ")
 
-    console.log(addressFromik);
-    useEffect(() => {}, [values]);
+
+  useEffect(() => {
+    setCities(City.getCitiesOfState("US", state))
+  }, [state]);
 
   return (
     <div className="App">
-        <form onSubmit={handleSubmit}>
-            <Select
-                id="state"
-                name="state"
-                label="state"
-                options={updatedStates}
-                value={values.state}
-                onChange={(value) => {
-                    setValues({ state: value, city: null});
-                }}
-            />
-            <Select
-            id="city"
-            name="city"
-            options={updatedCities(values.state ? values.state.value : 'AZ')}
-            value={values.city}
-            onChange={(value) => setFieldValue("city", value)}
-            />
-            {/* <p>{JSON.stringify(state)}</p> */}
-        </form>
-      {/* <form onSubmit={handleSubmit}>
-        <Select
-          id="city"
-          name="city"
-          options={updatedCities(values.state ? values.state.value : null)}
-          value={values.city}
-          onChange={(value) => setFieldValue("city", value)}
-        />
-        <button type="submit">Submit</button>
-        <p>{JSON.stringify(csc.get)}</p>
-      </form> */}
+      <Form.Select aria-label="select" onChange={handleStateSelect}>
+        {
+          states.map(
+            (state, index) => <option key={index} value={state.isoCode}>{state.name}</option>)
+        }
+      </Form.Select>
+      <Form.Select aria-label="select" onChange={handleCitySelect}>
+        {
+          cities && cities.map(
+            (city, index) => <option key={index} value={city.name}>{city.name}</option>)
+        }
+      </Form.Select>
     </div>
   );
 }
