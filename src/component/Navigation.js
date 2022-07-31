d0456518b706c23b76b0d0724f8ce27395bfd367
JiwonWoo1 <Jwoo@ggc.edu>
2022-07-31 02:19:35 -0400

    Done!

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import LoginButton from './buttons/LoginButton';
 import LogoutButton from './buttons/LogoutButton';
 import AddNewItemButton from './buttons/AddNewItemButton';
-import { useAuth0 } from '@auth0/auth0-react'
-import logo from './Images/logo.jpg'
-import logo1 from './Images/logo1.png'
+import { useAuth0 } from '@auth0/auth0-react';
+import logo from './Images/logo1.png';
 import ProfileButton from './buttons/ProfileButton';
+import '../styles/nav.css';
+
 const Navigation = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="white" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand className="m-auto" href="/">
             <img src={logo} />
